879deeaf2c80f3423439eaf59e3bb0c9b3908813
markpattison <mr.pattison@gmail.com>
2018-10-06 17:17:48 +0100

    redraw chart when returning to page

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -11,11 +11,15 @@ open Shared
 
 open Fulma
 
-type Model = { Values: Values option }
+type View = ShowChart | NoChart
+type Model = { View: View; Values: Values option; NeedToDrawChart: bool }
 
 type Msg =
-| RequestValues
-| ReceiveValues of Result<Values, exn>
+    | RequestValues
+    | ReceiveValues of Result<Values, exn>
+    | PageWithChart
+    | PageWithoutChart
+    | DrawChart
 
 module Server =
 
@@ -27,12 +31,10 @@ module Server =
       |> Remoting.withRouteBuilder Route.builder
       |> Remoting.buildProxy<IDemoApi>
 
-
 let init () : Model * Cmd<Msg> =
-    let initialModel = { Values = None }
-    initialModel, []
+    { View = ShowChart; Values = None; NeedToDrawChart = false }, []
 
-let loadCountCmd =
+let loadDataCmd =
     Cmd.ofAsync
         Server.api.demoValues
         ()
@@ -40,42 +42,49 @@ let loadCountCmd =
         (Error >> ReceiveValues)
 
 let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
-    match currentModel.Values, msg with
-    | _, RequestValues ->
-        currentModel, loadCountCmd
-    | _, ReceiveValues (Ok values)->
-        let nextModel = { Values = Some values }
-        let series = TheGamma.Series.series<_, _>.values values
-        let chart = TheGamma.GoogleCharts.chart.line series
-        TheGamma.GoogleCharts.chart.show chart "Chart1"
-        nextModel, []
-    | _ -> currentModel, []
+    match msg with
+    | RequestValues -> currentModel, loadDataCmd
+    | ReceiveValues (Ok values) -> { currentModel with Values = Some values }, Cmd.ofMsg DrawChart
+    | ReceiveValues (Error _) -> currentModel, []
+    | DrawChart ->
+        match currentModel.Values with
+        | Some values ->
+            let series = TheGamma.Series.series<_, _>.values values
+            let chart = TheGamma.GoogleCharts.chart.line series
+            TheGamma.GoogleCharts.chart.show chart "Chart1"
+        | None -> ()
+        { currentModel with NeedToDrawChart = false }, []
+    | PageWithChart -> { currentModel with View = ShowChart; NeedToDrawChart = true }, []
+    | PageWithoutChart -> { currentModel with View = NoChart }, []
 
 let button txt onClick =
     Button.button
-        [ Button.IsFullWidth
-          Button.Color IsPrimary
+        [ Button.Color IsPrimary
           Button.OnClick onClick ]
         [ str txt ]
 
-let show model =
-    match model.Values with
-    | Some values -> System.String.Join(",", values)
-    | None -> "Not loaded"
-
 let view (model : Model) (dispatch : Msg -> unit) =
+    let chartRef element =
+        if not (isNull element) && model.NeedToDrawChart then
+            dispatch DrawChart
+    let content =
+        match model.View with
+        | NoChart ->
+            div []
+              [ div [] [ str "Page without chart" ]
+                button "Show page with chart" (fun _ -> dispatch PageWithChart) ]
+        | ShowChart ->
+            div []
+              [ div [] [ str "Page with chart" ]
+                button "Get data" (fun _ -> dispatch RequestValues)
+                button "Show page without chart" (fun _ -> dispatch PageWithoutChart )
+                div [ Id "Chart1"; Ref chartRef ] [] ]
     div []
         [ Navbar.navbar [ Navbar.Color IsPrimary ]
             [ Navbar.Item.div [ ]
                 [ Heading.h2 [ ]
                     [ str "SAFE Template" ] ] ]
-
-          Container.container []
-              [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
-                    [ Heading.h3 [] [ str ("Values: " + show model) ] ]
-                Columns.columns []
-                    [ Column.column [] [ button "Get values" (fun _ -> dispatch RequestValues) ] ] ]
-          div [ Id "Chart1" ] [] ]
+          content ]
 
 #if DEBUG
 open Elmish.Debug
@@ -88,7 +97,4 @@ Program.mkProgram init update view
 |> Program.withHMR
 #endif
 |> Program.withReact "elmish-app"
-#if DEBUG
-// |> Program.withDebugger
-#endif
 |> Program.run
